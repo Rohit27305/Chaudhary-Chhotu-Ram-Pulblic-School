1d979300bc5c5558e2650a602dfe5d6860c3188f
Rohit Verma <133568861+Rohit27305@users.noreply.github.com>
2024-05-30 09:34:50 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { Carousel } from "./components/Carousel.jsx";
+import Carousel from "./components/Carousel";
+import FooterNavbar from "./components/FooterNavbar";
+import Hero from "./components/Hero";
+import SideBar from "./components/SideBar";
+import Tabs from "./components/Tabs";
+
+export default function App() {
+  return (
+    <>
+    <div className="">
+          <SideBar/>
+          <FooterNavbar/>
+          
+    </div>
+    </>
+  )
+}
